d55b5f5cfc3403363fae7bd420c3e6e55a7ad016
sergeykurilov <kurilovsergey15@gmail.com>
2023-08-27 23:54:35 +0300

    feat: initial config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CounterAppOne from "./components/CounterAppOne";
+
+const App = () => (
+	<div>
+		<div>APP-1</div>
+		<div>
+			<CounterAppOne />
+		</div>
+	</div>
+);
+
+export default App;
---
+++ src/components/CounterAppOne.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div >
+      <div>
+        Add by one each click <strong>APP-1</strong>
+      </div>
+      <div>Your click count : {count} </div>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+
+export default Counter;
